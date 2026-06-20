7a4ea13ad194fdb32b019535f6dc9d81d2499dbb
anfers1985 <andersonfernand3s@gmail.com>
2026-06-20 18:53:18 -0300

    update 20 6 2

--- worker.js
+++ worker.js
@@ -37,7 +37,7 @@ function corsHeaders(origin) {
   return {
     'Access-Control-Allow-Origin':  allowed,
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Headers': 'Content-Type, X-Admin-Secret',
     'Access-Control-Max-Age':       '86400',
     'Vary':                         'Origin'
   };
